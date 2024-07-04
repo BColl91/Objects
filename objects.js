08eeb0b27a1a679106402fd8459aba33b797217e
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-07-04 21:30:10 +0100

    1 out of 3

---
+++ objects.js
@@ -0,0 +1,63 @@
+// let person = {
+//     myName: "Bex",
+//     age: 32
+// };
+
+// console.log(person.myName);
+// console.log(person.age);
+// console.log(person["myName"])
+
+// let offer = "none";
+// let time = 1700;
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: [
+//         "Cappuccino", "Latte", "Filter coffee", "Tea", "Hot Chocolate"
+//     ],
+
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "No offer lol"
+// };
+// if (time < 1100){
+//     offer = cafe.breakfastOffer;
+//     // console.log(cafe.breakfastOffer);
+// } else if (time < 1500){
+//     offer = cafe.lunchOffer;
+//     // console.log(cafe.lunchOffer);
+// } else {offer = cafe.noOffer};
+// console.log(offer)
+// console.log(cafe)
+
+// let day = "Thursday";
+// let message = "";
+ 
+// let alarm = {
+//     weekendAlarm: "You are safe",
+//     weekdayAlarm: "GET UP"
+// }
+ 
+// if (day == "Saturday" & "Sunday"){
+//     message = alarm.weekendAlarm; 
+// } else {message = alarm.weekdayAlarm};
+// console.log(message)
+
+
+//Activity 1
+// const sayHi = () => {
+//     let person = {
+//         myName: "Bex",
+//         age: 32
+//     };
+//     console.log(`Hello, my name is ${person.myName}`);
+// };
+
+// sayHi();
+
+
+//Activity 2//Creat an object called pet with the key values of: name, typeOfPet, age, colour and methods called eat and drink. They should return a strign saying []
+
+//Activity 3
+//Create an object called coffeeShop with key values of: branch, drinks with prices, food with prices and methods called drinksOrdered and foodOrdered. They should return a string saying [You rOrder] is ... with all items chosen with costs and total cost.
